0f512733c163a5a89efc66152f99ddf04632b793
sagevedant <bhavevedant18@gmail.com>
2024-07-18 13:32:03 +0000

    container config

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //basically it means that all the below javascript code is written in new JS lang //
+
+// alert(3+3) it will still show error as we are writing in node not on browser //
+
+let name = "Vedant" //String
+let age = 19 //Number, there is one thing bigint to use big numbers
+let isLoggedIn = true //Boolean
+let state = null //null is not undefined it is empty
+// symbol it is unique
+
+console.log(typeof "Vedant" ) //to identify type of any thing
