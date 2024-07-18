5bfc8ebe2f7f199f61d9bbbb98f8e68c1bfdd3ed
sagevedant <bhavevedant18@gmail.com>
2024-07-18 13:16:15 +0000

    container config

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const totalSales = 1_50_00
+let CEOofcompany = "Vedant Bhave"
+// Var is not used now it is finally outdated//
+/*Constant once declared are declare hence it will not be changed later*/
+
+let revenue;
+//If we just leave the variable after declaring it, and then print it will only show undefined// 
+
+
+console.table([totalSales, CEOofcompany, revenue]);
+
+/* console.table([]) it is used to print multiple things in tabular format once directly */
