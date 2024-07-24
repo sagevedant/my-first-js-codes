e2c3caa5622f73fdf167df37deb6bc4c4f025670
sagevedant <bhavevedant18@gmail.com>
2024-07-24 16:06:15 +0000

    container config

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// Operations in java script
+
+console.log(2*2)  //it is the multiplication
+console.log(2**2); //it is 2 to the power 2
+console.log(2/2); //it is division
+console.log(100%9); // % sign is remainder
+
+// Adding the strings
+
+let str1 = "Hello"
+let str2 = " Vedant"
+let str3 = str1 + str2
+
+console.log(str3);
+
+// Error cases
