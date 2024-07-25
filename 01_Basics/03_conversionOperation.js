f7a79c8434cece23b39264a106af6a8fd80121f4
sagevedant <bhavevedant18@gmail.com>
2024-07-25 06:39:15 +0000

    container config

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -14,3 +14,7 @@ let str3 = str1 + str2
 console.log(str3);
 
 // Error cases
+
+console.log(1 + 2 + "5") //it will give 35 as numbers are first so it will treat initially as numbers hence it will add first two
+
+console.log("2" + 1 + 8); // it will give 218 as it is initialize with the string
